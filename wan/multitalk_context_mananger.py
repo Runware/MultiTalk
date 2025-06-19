e22782e8667fee5d32eabd7df340525fa13deaba
xziayro-ai <stephane.monte@runware.ai>
2025-06-19 08:53:07 +0000

    Fixin parallel cfg perf

--- wan/multitalk_context_mananger.py
+++ wan/multitalk_context_mananger.py
@@ -1,9 +1,11 @@
 import types
 from contextlib import contextmanager
+from regex import E
 from xfuser.core.distributed import get_sequence_parallel_world_size
 from xfuser.core.distributed import (
     init_distributed_environment,
     initialize_model_parallel,
+    get_sp_group
 )
 from .distributed.xdit_context_parallel import (
     usp_dit_forward_multitalk,
@@ -16,13 +18,13 @@ from torch import distributed as dist
 
 @contextmanager
 def parallel_context(pipeline, ulysses_size, ring_size, para_batch_size):
-    use_usp = ulysses_size * ring_size > 1
     model = pipeline.model
 
     original_attn_forwards = []
     original_crossattn_forwards = []
     original_model_forward = model.forward
 
+    use_usp = ulysses_size > 1 or ring_size > 1
     if use_usp or para_batch_size > 1:
         world_size=dist.get_world_size()
         assert ulysses_size * ring_size * para_batch_size == world_size, f"The number of ulysses_size and ring_size should be equal to the world size."
@@ -34,12 +36,20 @@ def parallel_context(pipeline, ulysses_size, ring_size, para_batch_size):
             world_size=world_size
         )
 
-        initialize_model_parallel(
-            classifier_free_guidance_degree=para_batch_size,
-            sequence_parallel_degree = ulysses_size * ring_size,
-            ring_degree=ring_size,
-            ulysses_degree=ulysses_size,
-        )
+        ## A bit of a hack but i didn't find a better way to check if the model parallel is already initialized
+        is_already_initialized = True
+        try:
+            get_sequence_parallel_world_size()
+        except Exception as e:
+            is_already_initialized = False
+
+        if not is_already_initialized:
+            initialize_model_parallel(
+                classifier_free_guidance_degree=para_batch_size,
+                sequence_parallel_degree = ulysses_size * ring_size,
+                ring_degree=ring_size,
+                ulysses_degree=ulysses_size,
+            )
 
         if use_usp:
             for block in model.blocks:
@@ -52,7 +62,7 @@ def parallel_context(pipeline, ulysses_size, ring_size, para_batch_size):
                     usp_crossattn_multi_forward_multitalk, block.audio_cross_attn
                 )
 
-            model.forward = types.MethodType(usp_dit_forward_multitalk, model)
+        model.forward = types.MethodType(usp_dit_forward_multitalk, model)
         sp_size = get_sequence_parallel_world_size()
     else:
         sp_size = 1
@@ -60,12 +70,11 @@ def parallel_context(pipeline, ulysses_size, ring_size, para_batch_size):
     try:
         yield sp_size
     finally:
-        if use_usp:
-            # Restore original attention forwards
-            for block, original_forward in zip(model.blocks, original_attn_forwards):
-                block.self_attn.forward = original_forward
-            for block, original_forward in zip(model.blocks, original_crossattn_forwards):
-                block.audio_cross_attn.forward = original_forward
+        # Restore original attention forwards
+        for block, original_forward in zip(model.blocks, original_attn_forwards):
+            block.self_attn.forward = original_forward
+        for block, original_forward in zip(model.blocks, original_crossattn_forwards):
+            block.audio_cross_attn.forward = original_forward
 
-            # Restore model forward and original sp flag
-            model.forward = original_model_forward
+        # Restore model forward and original sp flag
+        model.forward = original_model_forward
